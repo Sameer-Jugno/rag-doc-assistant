104b81eaf6cd71e32cc684093333bfdf1a1227cf
sameerjugno <sameerjugno3@gmail.com>
2026-06-13 16:53:28 +0500

    feat: complete recursive chunking pipeline with unified metadata

--- backend/app/services/ingestion.py
+++ backend/app/services/ingestion.py
@@ -1,5 +1,7 @@
 import os 
 import fitz 
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
+
 
 def extract_text_from_pdfs(path) : 
         
@@ -12,20 +14,43 @@ def extract_text_from_pdfs(path) :
             doc = fitz.open(f"{path}/{file}") 
             for page_num, page in enumerate(doc) : 
                 text = page.get_text().strip() 
+                
                 data.append({
-                    "source" : file, 
-                    "page" : page_num+1, 
+                    "metadata" : {
+                                "source" : file, 
+                                "page" : page_num+1, 
+                                "company" : file.split("-")[0]
+                            },
                     "page_content" : text
                 })
+            # company_name = file.split("-")[0]
+            # print(f"company : {company_name}")
     return data
+
+def chunk_documents(data) : 
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap = 200
+    )
+    texts = [txt['page_content'] for txt in data] 
+    meta_datas = [dt['metadata'] for dt in data]
+
+    chunks = splitter.create_documents(texts=texts, metadatas=meta_datas)
+    return chunks 
 
 
 if __name__ == "__main__" : 
+
+    # Step 1 : Text Extraction 
     path = "../data/raw"
     data = extract_text_from_pdfs(path) 
+    print("\n\n" , "-" * 100 )
     print("Length : ", len(data)) 
-    print("\n" , "-" * 50 )
-    print("First 10 Docs from pdfs : ", data[:1]) 
-    print("\n" , "-" * 50 )
-    print("Last 10 Docs from pdfs : ", data[626:]) 
+    print("First Doc from pdfs : ", data[:1]) 
 
+
+    # Step 2 : Chunking the Text
+    chunks = chunk_documents(data)
+    print("\n\n" , "-" * 100 )
+    print("Length : ", len(data)) 
+    print("Chunk_1 : ", chunks[0]) 
